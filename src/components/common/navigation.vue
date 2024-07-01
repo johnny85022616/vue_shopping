fb50d571873c6fe818073c0e415e37d2aed4020d
johnny_chang <johnny_chang@friday.tw>
2024-07-01 11:45:56 +0800

    完整版tailwind.config.js

--- src/components/common/navigation.vue
+++ src/components/common/navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navigation relative  h-[60px] m-0">
-    <div v-if="siteData" class="redBg isBsite w-full h-[60px] flex z-2 fixed px-5 box-border hover:cursor-pointer"
+    <div v-if="siteData" class="redBg isBsite w-full h-[60px] flex z-2 fixed px-5"
       :style="{ backgroundColor: `${siteData.headerColor ? siteData.headerColor : '#ddd5cc'}` }">
       <div class="mini-logo" @click="(evt) => openBsiteSite(evt)">
         <img :src="siteData.logoMobile" />
