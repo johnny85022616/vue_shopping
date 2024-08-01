a1df58b2f3dbd653553f3d85790d2cbf195cfc85
johnny_chang <johnny_chang@friday.tw>
2024-08-01 17:29:42 +0800

    加入prodcutType與productApi

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -2,11 +2,13 @@ import cart from './api_cart';
 import web from './api_web';
 import member from './api_member';
 import ai from './api_ai';
+import product from './ai_product';
 
 export default {
   cart,
   web,
   member,
-  ai
+  ai,
+  product
 }
 
