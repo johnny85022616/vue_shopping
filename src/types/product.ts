a32e33029edd3efa849b4e592616562751cda016
johnny_chang <johnny_chang@friday.tw>
2024-08-01 11:49:48 +0800

    新增productInterface

---
+++ src/types/product.ts
@@ -0,0 +1,18 @@
+export interface product {
+  productId: number;
+  name: string;
+  images: string;
+  marketPrice: number;
+  price: number;
+  promoPrice: number;
+  isQuantity: number;
+  isIntangible: number;
+  isStore: number;
+  campaignQty: number;
+  auto_category_id_L: string;
+  auto_category_id_L_c: string;
+  auto_category_id_M: string;
+  auto_category_id_M_c: string;
+  auto_category_id: string;
+  auto_category_id_c: string;
+}
