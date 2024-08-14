d755d91bfcf68ceb6def9e93bfe6a66f8b3e8cf3
johnny_chang <johnny_chang@friday.tw>
2024-08-14 17:52:43 +0800

    aiProducts 和 product type調整

--- src/types/product.ts
+++ src/types/product.ts
@@ -9,12 +9,12 @@ export interface product {
   isIntangible?: number;
   isStore?: number;
   campaignQty?: number;
-  auto_category_id_L?: string;
-  auto_category_id_L_c?: string;
-  auto_category_id_M?: string;
-  auto_category_id_M_c?: string;
-  auto_category_id?: string;
-  auto_category_id_c?: string;
+  auto_category_id_L: string;
+  auto_category_id_L_c: string;
+  auto_category_id_M: string;
+  auto_category_id_M_c: string;
+  auto_category_id: string;
+  auto_category_id_c: string;
   [key:string]:any //供之後vue添加屬性不報錯
 }
 export interface productsObj{
