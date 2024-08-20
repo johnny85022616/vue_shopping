04f49a75fdf0add1f1cf59fde1e91498ce262988
johnny_chang <johnny_chang@friday.tw>
2024-08-20 18:02:50 +0800

    menu tree ver1

---
+++ src/components/navigation/menutree.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="menutree">
+  </div>
+</template>
+
+<script lang="ts" setup name="menutree">
+import { ref } from 'vue';
+import { useBsiteStore } from '@/stores/bsiteStore';
+import { storeToRefs } from 'pinia';
+
+  const bsiteStore = useBsiteStore()
+  const {siteData} = storeToRefs(bsiteStore)
+
+  const isBsite = ref(false)
+  const bsiteShopItems = ref(null)
+  const shopItems = ref(null)
+  const bsiteShopItemsTitle = ref('全部分類')
+  const shopItemsTitle = ref('全部分類')
+
+  window.scrollTo(0, 0);
+    if (siteData.value) {
+      isBsite.value = true;
+
+      const { siteType, siteId } = siteData.value || {};
+      switch (siteType) {
+        // case "B4":
+        //   this.getBsiteCategorys(siteId);
+        //   break;
+        // case "B2":
+        //   this.getBsiteCategorys();
+        //   break;
+        // default:
+        //   this.bsiteShopItemsTitle = "本站商品分類";
+        //   this.shopItemsTitle = "友商推薦商品分類";
+        //   this.getBsiteCategorys(siteId);
+      }
+    } else {
+      // this.getBsiteCategorys();
+    }
+
+
+</script>
