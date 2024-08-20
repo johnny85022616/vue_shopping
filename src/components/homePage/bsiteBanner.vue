ec39b74610f40249ed3f3891dd016d3e4152b84f
johnny_chang <johnny_chang@friday.tw>
2024-08-20 16:16:23 +0800

    fix

--- src/components/homePage/bsiteBanner.vue
+++ src/components/homePage/bsiteBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bsiteBanner w-full flex items-center justify-around py-3">
-    <Carousel class="custom-carousel" :autoplay="3000" :loop="true" :autoplayTimeout="3000">
+    <Carousel class="custom-carousel" :autoplay="3000">
       <Slide class="wrap-item bg-c_background py-2" v-for="(item, index) of itemList" :key="index">
         <div class="banner-item w-full flex justify-around items-center">
           <a class="relative w-[48%] flex justify-center" v-for="(v, i) in item.data" :key="i" :href="v.link">
