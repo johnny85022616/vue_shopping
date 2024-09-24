6a96ca2949caf7044eb42ceeb607a2c8b4fdd263
johnny_chang <johnny_chang@friday.tw>
2024-09-24 17:59:03 +0800

    category router fix

--- src/router/friday.ts
+++ src/router/friday.ts
@@ -1,7 +1,6 @@
 import HomePage from '../views/Homepage.vue';
 import MemberCenter from '../views/MemberCenter.vue';
 import aiAllCategory from '../views/aiAllCategory.vue';
-import routerFunc from './routerFunc';
 
 export const fridayRoutes =  [
   {
@@ -14,40 +13,9 @@ export const fridayRoutes =  [
     name: 'memberCenter',
     component: MemberCenter
   },
-  // {
-  //   path: '/category/:lid',
-  //   name: 'aiAllCategory',
-  //   component: aiAllCategory,
-  //   beforeEnter: (to:any, from:any, next:any) => {
-  //     const lid = to.params.lid
-  //     if(/L[\d]+/.test(lid)){
-  //       next()
-  //     }else{
-  //       next({name:'home'})
-  //     }
-  //   }
-  // },
-  
   {
-    path: '/category/:catg1/:catg2/:catg3',
-    name: 'aiAllCategory',
-    component: (route:any)=>{routerFunc.categoryDirector(route.params)}
-   
-  },
-  {
-    path: '/category/:catg1/:catg2',
-    name: 'aiAllCategory',
-    component: (route:any)=>{routerFunc.categoryDirector(route.params)}
-   
-  },
-  {
-    path: '/category/:catg1',
+    path: '/category/:catg([LMB]{1}\\d{4})+',
     name: 'aiAllCategory',
     component: aiAllCategory,
-    beforeEnter: (to:any, from:any, next:any) => {
-      const params = to.params
-      const type = routerFunc.categoryDirector(params)
-      console.log(type); 
-    }
   },
 ]
