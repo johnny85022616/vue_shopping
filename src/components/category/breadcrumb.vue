0ce7d0409119204d431810484e84fa48c2faf12e
johnny_chang <johnny_chang@friday.tw>
2024-10-30 18:12:56 +0800

    新增麵包屑組件ver1

---
+++ src/components/category/breadcrumb.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="isShow" class="topnav">
+    <ul>
+      <li v-for="(item, index) of breadCrumbData" :key="index">
+        <a
+          href=""
+          @click.prevent="goPage(item.url)"
+          :class="{ last: index === breadCrumbData.length-1}"
+          >{{ item.name }}</a
+        >
+        <span v-if="index < breadCrumbData.length - 1">
+          <i class="arrow"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup name="aiAllCategory">
+import { ref } from 'vue';
+
+  const init = async()=>{
+
+  }
+
+  const isShow = ref(true)
+  const props = defineProps<{breadCrumbData: any[]}>()
+
+  const goPage = (url:string)=>{
+      location.href = url
+  }
+
+  init()
+
+</script>
+
