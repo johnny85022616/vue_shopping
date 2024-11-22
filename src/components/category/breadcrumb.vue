e2f9fa73bd5fc4a2a63dee0e31e4833cc4268b6a
johnny_chang <johnny_chang@friday.tw>
2024-11-22 18:13:27 +0800

    麵包屑加上css

--- src/components/category/breadcrumb.vue
+++ src/components/category/breadcrumb.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="topnav">
+  <div class="topnav block px-5 py-2 min-h-5 bg-c_background">
     <ul>
-      <li v-for="(item, index) of breadCrumbData" :key="index">
-        <RouterLink :class="{ last: index === breadCrumbData.length-1}" :to="{path: '/category/L0006'}">{{ item.name }}</RouterLink>
-        <span v-if="index < breadCrumbData.length - 1">
-          <i class="arrow"></i>
+      <li class="group inline list-none text-c_sliver" v-for="(item, index) of breadCrumbData" :key="index">
+        <RouterLink :class="'text-sm text-c_sliver no-underline group-last:text-c_mine_shaft'" :to="{path: item.url}">{{ item.name }}</RouterLink>
+        <span class="px-1 text-center align-middle" v-if="index < breadCrumbData.length - 1">
+          <i class="inline-block w-3 h-3"></i>
         </span>
       </li>
     </ul>
