6dd53b4fbd5bf8ea808735577f1739adf3615f14
張裕 <nte.82002@yahoo.com.tw>
2024-11-25 08:16:36 +0800

    update

---
+++ src/components/category/categoryMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script lang="ts" setup name="categoryMenu">
+import type { anyObject } from '@/types/common';
+import { ref } from 'vue';
+
+const props = withDefaults(defineProps<{
+  items: anyObject[]
+}>(),{
+  
+})
+
+  const init = async()=>{
+  }
+
+  init()
+
+</script>
