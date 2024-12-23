246c4d77d8ecfc976deaa4cfa54e84cd1b2c2c96
johnny_chang <johnny_chang@friday.tw>
2024-12-23 16:25:05 +0800

    新增搜尋頁路由及搜尋頁組件

--- src/router/friday.ts
+++ src/router/friday.ts
@@ -1,22 +1,34 @@
 import HomePage from '../views/Homepage.vue';
 import MemberCenter from '../views/MemberCenter.vue';
-import aiAllCategory from '../components/category/aiAllCategory.vue';
 import category from '../views/category.vue';
+import aiSearch from '../views/aiSearch.vue';
 
 export const fridayRoutes = [
+  //首頁路由
   {
     path: '/',
     name: 'home',
     component: HomePage,
   },
+  //會員中心路由
   {
     path: '/memberCenter',
     name: 'memberCenter',
     component: MemberCenter,
   },
+  //大中小目錄路由
   {
     path: '/category/:catg([LMB]{1}\\d{4})+',
     name: 'aiAllCategory',
     component: category,
+  },
+  //搜尋頁路由(queryString: keyword關鍵字)
+  {
+    path: '/aisearch',
+    name: 'aisearch',
+    component: aiSearch,
+    props: (route: any) => ({
+      query: route.query.keyword,
+    }),
   },
 ];
---
+++ src/views/aiSearch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="category">
+    <navigation :windowY="200" />
+    搜尋頁
+  </div>
+</template>
+
+<script lang="ts" setup name="category">
+import navigation from '@/components/common/navigation.vue';
+import api from "@/apis/api";
+import { ref, toRef } from "vue";
+import { useBsiteStore } from "@/stores/bsiteStore";
+import { storeToRefs } from "pinia";
+
+const bsiteStore = useBsiteStore();
+const { siteData } = storeToRefs(bsiteStore);
+const props = defineProps(['query'])
+// const {query} = toRef(props)
+
+const init = async () => {
+  console.log(props);
+}
+
+init()
+
+
+</script>
