219bf37aa4d5ad1a1d5fa31005ea0357b2ed875f
johnny_chang <johnny_chang@friday.tw>
2025-01-03 15:51:20 +0800

    改名

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -2,7 +2,7 @@ import cart from './api_cart';
 import web from './api_web';
 import member from './api_member';
 import ai from './api_ai';
-import product from './ai_product';
+import product from './api_product';
 import campaign from './api_campaign';
 import promotion from './api_promotion';
 export default {
