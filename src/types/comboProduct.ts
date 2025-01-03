4c892060f981d241a9452fac0c0e4dcc35a38407
johnny_chang <johnny_chang@friday.tw>
2025-01-03 15:53:02 +0800

    新增組合商品型態

---
+++ src/types/comboProduct.ts
@@ -0,0 +1,32 @@
+//組合商品api回來的組合商品型態
+export interface comboProduct {
+  comboId: number;
+  comboQty: number;
+  refProduct: RefProduct;
+}
+
+interface RefProduct {
+  name: string;
+  intro: string;
+  supplierId: number;
+  isSoldOut: boolean;
+  isIntangible: string;
+  sellSetId: number;
+  pid: number;
+  images: string[];
+  videos: any[];
+  tags: string[];
+  variants: Variant[];
+  promotionLabels: any[];
+}
+
+interface Variant {
+  groupTitle: string;
+  name: string;
+  id: number;
+  qty: number;
+  isSoldOut: boolean;
+  quantityLevel: string;
+  purchaseMinQty: number;
+  qtyMax: number;
+}
