e69d675e2c9ba8dd7969bfcb0c463a90d8956e56
johnny_chang <johnny_chang@friday.tw>
2025-01-06 11:58:33 +0800

    新增單品頁專用單品完整資料型態

---
+++ src/types/productInfo.ts
@@ -0,0 +1,45 @@
+//單品完整資料(單品頁使用)
+export interface productInfo {
+  name: string;
+  supplierId: number;
+  isSoldOut: boolean;
+  isIntangible: string;
+  price: Price;
+  campaignFlags: CampaignFlags;
+  sellSetId: number;
+  shipmentPreserveId: number;
+  startSaleTime: string;
+  isSale: boolean;
+  isFullRedeem: boolean;
+  pid: number;
+  images: string[];
+  videos: string[];
+  tags: string[];
+  variants: Variant[];
+  promotionLabels: any[];
+}
+
+interface Variant {
+  groupTitle: string;
+  name: string;
+  id: number;
+  qty: number;
+  isSoldOut: boolean;
+  quantityLevel: string;
+  purchaseMinQty: number;
+  qtyMax: number;
+}
+
+interface CampaignFlags {
+  k: string;
+  v: (string[] | null)[];
+}
+
+interface Price {
+  memberPrice: number;
+  marketPrice: number;
+  promoPrice: null;
+  bestDiscountA: number;
+  bestDiscountO: number;
+  bestDiscountPrice: number;
+}
