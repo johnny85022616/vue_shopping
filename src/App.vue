0b3c964d58d8f86d788fa693c8233742d500f558
johnny_chang <johnny_chang@friday.tw>
2025-01-07 14:47:17 +0800

    將vue transiition css 搬到 App.vue 中的style區塊

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,14 @@ import { RouterView } from 'vue-router'
 
 <style lang='scss'>
 @import './style/reset';
+
+.slide-enter-active, .slide-leave-active {
+    @apply transition-transform duration-100 ease-linear
+  }
+  .slide-enter-from, .slide-leave-to {
+    @apply translate-y-full
+  }
+  .slide-enter-to, .slide-leave-from {
+    @apply translate-y-0
+  }
 </style>
