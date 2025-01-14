f02a338b51ad05df610675e7ef63824de2a8072b
johnny_chang <johnny_chang@friday.tw>
2025-01-14 15:25:23 +0800

    將取得fridayData和siteData邏輯搬到router config檔中解決

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,17 @@
-import { createApp, inject } from 'vue'
-import { createPinia } from 'pinia'
-import VueCookies from 'vue-cookies'
-import {useBsiteStore} from './stores/bsiteStore';
-import tools from '@/util/tools';
-import App from './App.vue'
-import router from './router'
-import './index.css'
+import { createApp, inject } from 'vue';
+import { createPinia } from 'pinia';
+import VueCookies from 'vue-cookies';
+// import { useBsiteStore } from './stores/bsiteStore';
+import App from './App.vue';
+import router from './router';
+import './index.css';
 
-const app = createApp(App)
-app.provide('tools',tools)
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
 app.use(VueCookies);
-const bsiteStore = useBsiteStore()
-bsiteStore.getSiteData().then(()=>{
-  app.mount('#app')
-})
-
-
+// const bsiteStore = useBsiteStore();
+// bsiteStore.getSiteData().then(() => {
+//   app.mount('#app');
+// });
+app.mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import {fridayRoutes} from './friday';
-import {bsiteRoutes} from './bsite';
+import { createRouter, createWebHistory } from 'vue-router';
+import { fridayRoutes } from './friday';
+import { bsiteRoutes } from './bsite';
+import { useBsiteStore } from '@/stores/bsiteStore';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    ...fridayRoutes,
-    ...bsiteRoutes
-  ]
-})
+  routes: [...fridayRoutes, ...bsiteRoutes],
+});
 
-export default router
+router.beforeEach(async (to, from, next) => {
+  try {
+    const bsiteStore = useBsiteStore();
+    //若之前取過不需要再取一次
+    if (window.fridayData || bsiteStore.siteData) {
+      next();
+      return;
+    }
+
+    await bsiteStore.getSiteData();
+    if (window.fridayData || bsiteStore.siteData) {
+      next();
+    }
+  } catch (e) {
+    alert('取得供應商發生錯誤');
+  }
+});
+export default router;
