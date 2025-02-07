167c00dcceeed5c921ef020c1c831f50f9142735
johnny_chang <johnny_chang@friday.tw>
2025-02-07 17:47:34 +0800

    productInfo 新增 couponCategory

--- src/types/productInfo.ts
+++ src/types/productInfo.ts
@@ -17,6 +17,7 @@ export interface productInfo {
   tags: string[];
   variants: Variant[];
   promotionLabels: any[];
+  couponCategory?: couponCategory;
   [key: string]: any; //供之後vue添加屬性不報錯
 }
 
@@ -43,4 +44,74 @@ interface Price {
   bestDiscountA: number;
   bestDiscountO: number;
   bestDiscountPrice: number;
+}
+
+interface couponCategory {
+  [key: string]: info;
+}
+
+interface info {
+  tagTitle: string;
+  data: couponCategoryData[];
+}
+
+interface couponCategoryData {
+  campaignId: string;
+  campaignName: string;
+  campaignStartTimestamp: string;
+  campaignEndTimestamp: string;
+  offerCondition: string[];
+  offerContents: OfferContents;
+  campaignRange: ProductRange;
+  otherInformation: OtherInformation;
+  ui: Ui;
+}
+
+interface Ui {
+  digitalType: string;
+  discountAmount: number;
+  discountCode: string;
+  isAnti: boolean;
+  isGeted: boolean;
+  title: string;
+  campaignPageTitle: string;
+  rule: string;
+  campaignId: string;
+  campaignName: string;
+  campaignEndTime: string;
+  couponExpireTime: string;
+  listCampaignEndTime: string;
+  campaignUrl: string;
+  isShowInMyCampaign: boolean;
+  isAdditionalDiscount: boolean;
+}
+
+interface OtherInformation {
+  imgUrl: string;
+  image: string;
+  hyperlink: string;
+  campSiteUrl: string;
+  campUrl: string;
+  fakeCate: string;
+  mask: number;
+}
+
+interface OfferContents {
+  d5: D5;
+  digitalSignal: string;
+}
+
+interface D5 {
+  minAmount: string;
+  password: string;
+  quantity: string;
+  d: string;
+  productRange: ProductRange;
+  discount: string[];
+  couponId: number;
+}
+
+interface ProductRange {
+  v: string[];
+  k: string;
 }
