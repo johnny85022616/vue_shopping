a31ea9be5478525b1ff6a96da923629c3bbe5b2a
johnny_chang <johnny_chang@friday.tw>
2025-02-19 17:23:32 +0800

    新增方法

--- src/apis/api_campaign.ts
+++ src/apis/api_campaign.ts
@@ -4,6 +4,40 @@ import { getCampaignUI } from './campaign/campaign_util';
 
 const { isLogin, cloudApiPath, fetchGetHeaders, fetchPostHeaders, frontApiPath } = config;
 
+const responseHandler = (res: any): { status: number; msg: string; code: number } => {
+  const { resultData } = res;
+  const { msg, resultCode } = resultData[0];
+  const code = parseInt(resultCode);
+
+  if (res.resultCode === 999) {
+    return {
+      status: 0,
+      msg: '請先登入會員！',
+      code,
+    };
+  }
+  if (res.resultCode === 0) {
+    if (parseInt(resultCode) === 0) {
+      return {
+        status: 1,
+        msg: msg,
+        code,
+      };
+    } else {
+      return {
+        status: 0,
+        msg: msg,
+        code,
+      };
+    }
+  }
+  return {
+    status: 0,
+    msg: msg,
+    code,
+  };
+};
+
 const api_campaign = {
   // 取活動明細
   async getCampaignDetail(campaignIds: string[]) {
@@ -86,7 +120,7 @@ const api_campaign = {
           return v;
         });
   },
-
+  // 取得個人身上有哪些活動折扣campaignId
   async getMyCampaigns(returnDetail = false): Promise<string[]> {
     if (!isLogin) return [];
     return await fetch(`${frontApiPath()}api/campaign/memCampaign`, {
@@ -105,6 +139,28 @@ const api_campaign = {
           return ui;
         }
         return ids;
+      });
+  },
+  // 領取折扣碼
+  async drawDiscountCode(discountCode = '') {
+    if (!isLogin) {
+      return {
+        status: 0,
+        msg: '請先登入會員！',
+      };
+    }
+    return await fetch(`${frontApiPath()}api/campaign/drawDiscountCode`, {
+      ...fetchPostHeaders,
+      body: JSON.stringify({
+        param: {
+          password: discountCode,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        window.sessionStorage.removeItem('my_campaign_count');
+        return responseHandler(res);
       });
   },
 };
