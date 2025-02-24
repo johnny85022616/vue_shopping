b3d406f95a4fd4d06dc23b11fd0c2f1dc3a58949
johnny_chang <johnny_chang@friday.tw>
2025-02-24 18:10:41 +0800

    新增關鍵字keyword型態

---
+++ src/types/keyword.ts
@@ -0,0 +1,35 @@
+interface keyword {
+  target: string;
+  target_value: string;
+  target_ss: string;
+  list_fun: string;
+  list_args: string;
+  list_remote: string;
+  list_cmpn: string;
+  flag_id: number;
+  flag_code: number;
+  flag_info: string;
+  num: number;
+  kids: Kid[];
+  kids_show: Kid[];
+  pids: Pid[];
+}
+
+export interface Pid {
+  pid: string;
+  kids_show: Kid2[];
+  kids: Kid2[];
+}
+
+export interface Kid2 {
+  kid: number;
+  kcontent: string;
+}
+
+export interface Kid {
+  [key: string]: kids;
+}
+
+export interface kids {
+  [key: string]: string;
+}
