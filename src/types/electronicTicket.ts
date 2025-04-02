274dc2d787f9eae6229731248f6c29c375b568d4
johnny_chang <johnny_chang@friday.tw>
2025-04-02 15:15:27 +0800

    新增電子票券type

---
+++ src/types/electronicTicket.ts
@@ -0,0 +1,10 @@
+export interface electronicTicket {
+  dealId: string;
+  productId: string;
+  productName: string;
+  images: string;
+  manufacturerCode: string;
+  sn: string;
+  barcode: string;
+  period: string;
+}
