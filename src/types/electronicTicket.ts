f2b81a1648679dfcac9f5803199101a68a8a3154
johnny_chang <johnny_chang@friday.tw>
2025-04-14 09:43:49 +0800

    fix

--- src/types/electronicTicket.ts
+++ src/types/electronicTicket.ts
@@ -1,10 +1,12 @@
+//電子票券
 export interface electronicTicket {
   dealId: string;
   productId: string;
   productName: string;
-  images: string;
   manufacturerCode: string;
+  images: string;
   sn: string;
   barcode: string;
   period: string;
+  isMemoed: boolean;
 }
