84fbd0ac0f30576b06a6f8a028615e481f173401
johnny_chang <johnny_chang@friday.tw>
2025-05-13 14:39:47 +0800

    format

--- src/hooks/useConsignee.ts
+++ src/hooks/useConsignee.ts
@@ -1,39 +1,39 @@
-import { ref } from "vue";
+import { ref } from 'vue';
 import api from '@/apis/api';
-import type { consignee } from "@/types/consignee";
-import type { OrNull } from "@/types/util";
+import type { consignee } from '@/types/consignee';
+import type { OrNull } from '@/types/util';
 import usePopup from '@/hooks/usePopup';
 
-export default function useConsignee(){
+export default function useConsignee() {
   const consigneeData = ref<OrNull<consignee[]>>(null); //收貨人資料
-  const popup = usePopup() //popup composable
+  const popup = usePopup(); //popup composable
 
   //取得收貨人資料
-async function getConsignee() {
-  api.member.getConsignee(true).then((data) => {
-    if (data) {
-      consigneeData.value = data
-    }
-  });
-}
-//變更預設收貨人
-async function updateDefaultConsignee(id: string){
-  const pass = await api.member.updateDefaultConsignee(id);
-  if(pass) getConsignee()
-}
+  async function getConsignee() {
+    api.member.getConsignee(true).then((data) => {
+      if (data) {
+        consigneeData.value = data;
+      }
+    });
+  }
+  //變更預設收貨人
+  async function updateDefaultConsignee(id: string) {
+    const pass = await api.member.updateDefaultConsignee(id);
+    if (pass) getConsignee();
+  }
 
-//刪除收貨人
-async function deleteConsignee(id: string){
-  const confirm = await popup.confirm('是否刪除此收貨人')
-  if(!confirm) return 
-  const pass = await api.member.deleteConsignee(id);
-  if(pass) getConsignee()
-}
+  //刪除收貨人
+  async function deleteConsignee(id: string) {
+    const confirm = await popup.confirm('是否刪除此收貨人');
+    if (!confirm) return;
+    const pass = await api.member.deleteConsignee(id);
+    if (pass) getConsignee();
+  }
 
-return {
-  consigneeData,
-  getConsignee,
-  updateDefaultConsignee,
-  deleteConsignee
-}
+  return {
+    consigneeData,
+    getConsignee,
+    updateDefaultConsignee,
+    deleteConsignee,
+  };
 }
