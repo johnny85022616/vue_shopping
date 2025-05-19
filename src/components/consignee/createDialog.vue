5f782b795fd2d057ad0d6e7b95555d8604f52fb2
johnny_chang <johnny_chang@friday.tw>
2025-05-19 16:35:46 +0800

    form表單錯誤樣式

--- src/components/consignee/createDialog.vue
+++ src/components/consignee/createDialog.vue
@@ -10,28 +10,28 @@
             <i v-if="isDefault" class="consigneeForm__preset-on" @click="defaultSwitch"></i>
             <i v-else class="consigneeForm__preset-off" @click="defaultSwitch"></i>
           </div> -->
-            <div :class="['mb-5', { error: nameAlert.length > 0 }]">
+            <div :class="['mb-5 group', { error: nameAlert.length > 0 }]">
               <p class="text-c_heavy-metal">*姓名</p>
-              <input type="text" class="formInput" v-model="name" placeholder="請輸入姓名" />
-              <span class="">{{ nameAlert }}</span>
+              <input type="text" class="formInput group-[.error]:border-c_red" v-model="name" placeholder="請輸入姓名" />
+              <span class="text-c_red">{{ nameAlert }}</span>
             </div>
-            <div :class="['mb-5', { error: phoneAlert.length > 0 }]">
+            <div :class="['mb-5 group', { error: phoneAlert.length > 0 }]">
               <p class="text-c_heavy-metal">*手機號碼</p>
-              <input type="phone" class="formInput" v-model="phone" @input="phoneFormat" placeholder="請輸入手機號碼" />
-              <span>{{ phoneAlert }}</span>
+              <input type="phone" class="formInput group-[.error]:border-c_red" v-model="phone" @input="phoneFormat" placeholder="請輸入手機號碼" />
+              <span class="text-c_red">{{ phoneAlert }}</span>
             </div>
-            <div :class="['consigneeForm__address mb-5 ', { error: addressAlert.length > 0 }]">
+            <div :class="['consigneeForm__address mb-5 group', { error: addressAlert.length > 0 }]">
               <p class="text-c_heavy-metal">*地址</p>
               <div class="flex">
-                <select class="formSelect mr-5" name="city" v-model="city" @change="city && changeCity(city)">
+                <select class="formSelect group-[.error]:border-c_red mr-5" name="city" v-model="city" @change="city && changeCity(city)">
                   <option v-for="(item, index) in cityArr" :key="index" :value="item.id">{{ item.name }}</option>
                 </select>
-                <select class="formSelect" name="region" v-model="region">
+                <select class="formSelect group-[.error]:border-c_red" name="region" v-model="region">
                   <option v-for="(item, index) in regionArr" :key="index" :value="item.id">{{ item.name }}</option>
                 </select>
               </div>
-              <input type="text" class="formInput" v-model="road" placeholder="請輸入地址" />
-              <span>{{ addressAlert }}</span>
+              <input type="text" class="formInput group-[.error]:border-c_red" v-model="road" placeholder="請輸入地址" />
+              <span class="text-c_red">{{ addressAlert }}</span>
             </div>
             <p class="text-sm text-c_heavy_metal">
               按下確認即表示同意將本次收貨人資料紀錄於「收貨人管理｣中，節省下次結帳時間。
@@ -52,10 +52,12 @@ import fullscreenDialog from '@/components/common/fullscreenDialog.vue';
 import useMemberForm from '@/hooks/useMemberForm';
 import { ref, toRefs, watch } from 'vue';
 
-const { memberForm,changeCity, phoneFormat } = useMemberForm();
+const { memberForm,changeCity, phoneFormat, formCheck} = useMemberForm();
 const { name, phone, city, region, road, nameAlert, phoneAlert, addressAlert, cityArr, regionArr } = toRefs(memberForm);
 const isDefault = ref(false);
 
-function confirmClick() { }
+function confirmClick() {
+  formCheck(['name', 'phone', 'address'])
+ }
 
 </script>
