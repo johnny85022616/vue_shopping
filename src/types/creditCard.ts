9441e803ff6d8a7633ad1f3d748cf810767fc644
johnny_chang <johnny_chang@friday.tw>
2025-05-20 12:05:12 +0800

    新增creditCard type

---
+++ src/types/creditCard.ts
@@ -0,0 +1,14 @@
+interface creditCard {
+  number: string;
+  name: string;
+  expiration: string;
+  cardName: string;
+  isDefault: string;
+  expressCheckoutId: string;
+  bankId: string;
+  bankName: string;
+  creditCardType: number;
+  lastFourDigits: string;
+  temp: boolean;
+  isChose?: boolean;
+}
