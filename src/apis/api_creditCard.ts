04c09dbcce74006e47fc0da357274ca03ca0f48e
johnny_chang <johnny_chang@friday.tw>
2025-05-20 14:16:14 +0800

    add type

--- src/apis/api_creditCard.ts
+++ src/apis/api_creditCard.ts
@@ -1,9 +1,10 @@
+import type { creditCard } from '@/types/creditCard';
 import config from '../config/config';
 const { frontApiPath, fetchGetHeaders,fetchPostHeaders } = config
 const frontPath = frontApiPath()
 const api_creditCard = {
   //查詢信用卡列表
-  async getCreditCard() {
+  async getCreditCard(): Promise<creditCard[]> {
     return await fetch(`${frontPath}mgmt/member/creditcard`, {
       ...fetchPostHeaders,
     })
