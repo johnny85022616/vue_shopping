832911391ac69f5aea5b7673c7fa3232514a43e8
johnny_chang <johnny_chang@friday.tw>
2025-05-20 11:47:00 +0800

    新增creditCard 相關檔案

---
+++ src/components/creditCard/creditCardList.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="consigneeList bg-c_background min-h-[100vh] mb-[50px] py-3">
+    <li v-for="item in consigneeData" class="bg-c_white flex justify-between py-4 px-2 m-2 rounded-lg first:mt-0"
+      :key="item.id">
+      <div class="consigneeList__item-left">
+        <div class="flex mb-2">
+          <i :class="`${item.isDefaultBoolean ? 'bg-radio-active-icon' : 'bg-radio-icon'
+            } w-5 h-5 mr-2 bg-center bg-100% bg-no-repeat`" @click="updateConsignee(item.id)"></i>
+          <p class="text-sm font-bold text-c_black">{{ item.name }}</p>
+          <p v-if="item.isDefault" class="text-sm font-bold text-c_black">(預設)</p>
+        </div>
+        <p class="text-sm mb-1 text-c_sliver font-bold">{{ item.mobile }}</p>
+        <p class="text-sm text-c_sliver font-bold mb-2">{{ item.addr?.fullAddress }}</p>
+      </div>
+      <div class="consigneeList__item-right">
+        <i @click="deleteConsignee(item.id)"
+          class="inline-block bg-delete-icon w-5 h-5 mr-2 bg-center bg-100% bg-no-repeat"></i>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup name="consigneeList">
+import type { consigneeConposable } from '@/hooks/useConsignee';
+import type { consignee } from '@/types/consignee';
+import { inject, toRefs } from 'vue';
+
+const props = defineProps<{
+  consigneeData: consignee[];
+}>();
+const { consigneeData } = toRefs(props);
+const consignee = inject('consignee') as consigneeConposable;
+
+const emit = defineEmits(['updateDefaultConsignee']);
+
+init();
+
+function init() { }
+
+//變更預設收貨人
+function updateConsignee(id: string) {
+  consignee.updateDefaultConsignee(id);
+}
+//刪除聯絡人
+async function deleteConsignee(id: string) {
+  consignee.deleteConsignee(id);
+}
+</script>
---
+++ src/views/CreditCardManagement.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="creditCardManagement">
+    <navigation :windowY="200"></navigation>
+    <div class="w-full h-[50px] text-center border-b border-solid border-c_alto relative flex items-center justify-between p-5">
+      <i class="w-5 h-5 bg-nextArrow-icon bg-center bg-100% bg-no-repeat rotate-180 " @click="historyBack"></i>
+      <p class="text-lg font-bold text-c_mine_shaft">信用卡管理</p>
+      <span @click="openCreateDialog">新增</span>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup name="creditCardManagement">
+import navigation from '@/components/common/navigation.vue';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+function historyBack() {
+ router.back()  
+}
+
+function openCreateDialog() {
+}
+</script>
