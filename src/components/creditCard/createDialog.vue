c059414b7baa424506fbe5478957324601d8699f
johnny_chang <johnny_chang@friday.tw>
2025-05-21 14:17:49 +0800

    新增信用卡ui ver1

--- src/components/creditCard/createDialog.vue
+++ src/components/creditCard/createDialog.vue
@@ -10,9 +10,14 @@
               <input type="text" class="formInput group-[.error]:border-c_red" v-model="name" placeholder="請輸入姓名" />
               <span class="text-c_red">{{ nameAlert }}</span>
             </div>
+            <div :class="['mb-5 group', { error: creditNumAlert.length > 0 }]">
+              <p class="text-c_heavy-metal">信用卡號碼</p>
+              <input type="text" class="formInput group-[.error]:border-c_red" v-model="creditNum" placeholder="XXXX XXXX XXXX XXXX" />
+              <span class="text-c_red">{{ creditNumAlert }}</span>
+            </div>
           </div>
           <div
-            class="consigneeForm__submitBtn w-full h-[60px] leading-[60px] fixed bottom-0 left-0 right-0 bg-c_white text-center border-t border-solid border-c_alto">
+            class="submitBtn w-full h-[60px] leading-[60px] fixed bottom-0 left-0 right-0 bg-c_white text-center border-t border-solid border-c_alto">
             <span class="inline-block w-[92%] h-[40px] bg-c_red text-c_white leading-[40px] text-center rounded-[10px]"
               @click="confirmClick">確認</span>
           </div>
@@ -23,32 +28,24 @@
 </template>
 <script lang="ts" setup name="createDialog">
 import fullscreenDialog from '@/components/common/fullscreenDialog.vue';
-import type { consigneeConposable } from '@/hooks/useConsignee';
 import useMemberForm from '@/hooks/form/useMemberForm';
 import { inject, ref, toRefs, watch } from 'vue';
+import type { creditCardConposable } from '@/hooks/useCreditCard';
+import useCreditCardForm from '@/hooks/form/useCreditCardForm';
 
-const { memberForm, changeCity, phoneFormat, formCheck } = useMemberForm();
-const consignee = inject('consignee') as consigneeConposable;
-const closeDialog:any = inject('closeDialog')
-const { name, phone, city, region, road, nameAlert, phoneAlert, addressAlert, cityArr, regionArr } = toRefs(memberForm);
+const { memberForm, formCheck:checkMemeber } = useMemberForm();
+const { creditCardForm, checkCreditCard } = useCreditCardForm();
+const creditCard = inject('creditCard') as creditCardConposable;
+const closeDialog: any = inject('closeDialog')
 const isDefault = ref(true);
+const { name, nameAlert } = toRefs(memberForm)
+const { creditNum, creditNumAlert } = toRefs(creditCardForm)
 
-//form檢查 ＆ 新增收貨人
+
 function confirmClick() {
-  const isPass = formCheck(['name', 'phone', 'address'])
-  if (!isPass) return
-  let postData = {
-    data: {
-      addr: road.value.replace(/\s*/g, ""),
-      city: city.value,
-      county: region.value,
-      isDefault: isDefault.value ? "Y" : "N",
-      mobile: phone.value,
-      name: name.value,
-    },
-  };
-  consignee.createConsignee(postData);
-  closeDialog()
+  const namePass = checkMemeber(['name']); 
+  const creditPass = checkCreditCard()
+  return namePass && creditPass 
 }
 
 </script>
