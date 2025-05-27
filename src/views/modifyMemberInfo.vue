f5ccd06e34dd0db64d03dbbbb08fea68bd2feda4
johnny_chang <johnny_chang@friday.tw>
2025-05-27 15:18:46 +0800

    查看會員資料頁、姓名、電話地址ui &相關驗證

--- src/views/modifyMemberInfo.vue
+++ src/views/modifyMemberInfo.vue
@@ -1,9 +1,63 @@
 <template>
   <div class="modifyMemberInfo">
     <navigation></navigation>
+    <div
+      class="w-full h-[50px] text-center border-b border-solid border-c_alto relative flex items-center justify-center p-5">
+      <i class="w-5 h-5 bg-nextArrow-icon bg-center bg-100% bg-no-repeat rotate-180 justify-self-start absolute left-2"
+        @click="historyBack"></i>
+      <p class="text-lg font-bold text-c_mine_shaft">查看會員資料</p>
+    </div>
+    <div class="consigneeForm mt-2 mr-4 mb-20 ml-4">
+      <div :class="['mb-5 group', { error: nameAlert.length > 0 }]">
+        <p class="text-c_heavy-metal">姓名</p>
+        <input type="text" class="formInput group-[.error]:border-c_red" v-model="name" placeholder="請輸入姓名" />
+        <span class="text-c_red">{{ nameAlert }}</span>
+      </div>
+      <div :class="['mb-5 group', { error: phoneAlert.length > 0 }]">
+        <p class="text-c_heavy-metal">手機號碼</p>
+        <input type="phone" class="formInput group-[.error]:border-c_red" v-model="phone" @input="phoneFormat"
+          placeholder="請輸入手機號碼" />
+        <span class="text-c_red">{{ phoneAlert }}</span>
+      </div>
+      <div :class="['consigneeForm__address mb-5 group', { error: addressAlert.length > 0 }]">
+        <p class="text-c_heavy-metal">地址</p>
+        <div class="flex">
+          <select class="formSelect group-[.error]:border-c_red mr-5" name="city" v-model="city"
+            @change="city && changeCity(city)">
+            <option v-for="(item, index) in cityArr" :key="index" :value="item.id">{{ item.name }}</option>
+          </select>
+          <select class="formSelect group-[.error]:border-c_red" name="region" v-model="region">
+            <option v-for="(item, index) in regionArr" :key="index" :value="item.id">{{ item.name }}</option>
+          </select>
+        </div>
+        <input type="text" class="formInput group-[.error]:border-c_red" v-model="road" placeholder="請輸入地址" />
+        <span class="text-c_red">{{ addressAlert }}</span>
+      </div>
+    </div>
+    <div class="flex justify-center">
+      <span class="inline-block w-[92%] h-[40px] bg-c_red text-c_white leading-[40px] text-center rounded-[10px]"
+        @click="confirmClick">確認</span>
+    </div>
   </div>
 </template>
 <script lang="ts" setup name="modifyMemberInfo">
 import api from "@/apis/api";
 import navigation from "../components/common/navigation.vue";
+import { useRouter } from "vue-router";
+import useMemberForm from "@/hooks/form/useMemberForm";
+import { toRefs } from "vue";
+
+const router = useRouter()
+const { memberForm, changeCity, phoneFormat, formCheck } = useMemberForm();
+const { name, phone, city, region, road, nameAlert, phoneAlert, addressAlert, cityArr, regionArr } = toRefs(memberForm);
+
+function historyBack() {
+  router.back()
+}
+
+//form檢查 ＆ 新增收貨人
+function confirmClick() {
+  const isPass = formCheck(['name', 'phone', 'address'])
+  if (!isPass) return
+}
 </script>
