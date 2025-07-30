671f354619259428e2303ca6bf98598cf00af963
johnny_chang <johnny_chang@friday.tw>
2025-07-30 10:39:47 +0800

    簡化reset button 樣式判斷

--- src/components/invoice/invoiceDialog.vue
+++ src/components/invoice/invoiceDialog.vue
@@ -32,14 +32,16 @@
                 <span v-show="!companyVat" class="error-msg text-c_red ">輸入格式錯誤，請重新輸入。</span>
               </div>
               <p class="text-c_sliver mt-5">如您需紙本發票，請至【訂單查詢】點選「發票資訊」直接下載列印PDF。</p>
-              <p class="text-c_sliver mt-5">依統一發票使用辦法規定：電子發票一經開立，不得任意更改或改開公司發票。(<a  class="text-c_dodger_blue" href="https://www.einvoice.nat.gov.tw/" target="_blank">財政部電子發票流程說明</a>)</p>
+              <p class="text-c_sliver mt-5">依統一發票使用辦法規定：電子發票一經開立，不得任意更改或改開公司發票。(<a class="text-c_dodger_blue"
+                  href="https://www.einvoice.nat.gov.tw/" target="_blank">財政部電子發票流程說明</a>)</p>
             </div>
           </div>
         </div>
         <div
           class="submit-area absolute bottom-0 left-0 w-full p-2 border-t border-solid border-c_alto text-center bg-c_white flex justify-between items-center">
           <button
-            :class="`w-[49%] p-2 flex justify-center items-center text-base rounded-[10px] border border-solid ${isVehicleSetting ? 'border-c_red' : 'border-c_sliver'} ${isVehicleSetting ? 'text-c_red' : 'text-c_sliver'}`" @click="resetInvoice(currenType)">重置</button>
+            :class="`w-[49%] p-2 flex justify-center items-center text-base rounded-[10px] border border-solid ${resetBtnBorderColor} ${resetBtnTextColor}`"
+            @click="resetInvoice(currenType)">重置</button>
           <button
             class="w-[49%] p-2 flex justify-center items-center text-base text-c_white bg-c_red rounded-[10px] border border-solid border-c_red"
             @click="updateInvoice(currenType)">確認</button>
@@ -59,8 +61,10 @@ const vehicle = ref("") // 手機條碼輸入框
 const companyVat = ref("") //公司統一編號輸入框
 const companyName = ref("") //公司名稱
 const isVehicleValid = ref(true) // 驗證手機條碼載具是否正確
+const resetBtnBorderColor = ref('border-c_red')
+const resetBtnTextColor = ref('text-c_red')
 
-const props = withDefaults(defineProps<{originVehicle: string, oirginCompanyVat: string, originCompanyName: string, currenType: number, isVehicleSetting: boolean, isCompanySetting:boolean, invoiceItems: { typeName: string; typeInfo: string; type: number }[] | null }>(), {
+const props = withDefaults(defineProps<{ originVehicle: string, oirginCompanyVat: string, originCompanyName: string, currenType: number, isVehicleSetting: boolean, isCompanySetting: boolean, invoiceItems: { typeName: string; typeInfo: string; type: number }[] | null }>(), {
   originVehicle: "", //之前設定的手機條碼載具
   oirginCompanyVat: "", //之前設定的統一編號
   originCompanyName: "", //之前設定的公司名稱
@@ -69,40 +73,29 @@ const props = withDefaults(defineProps<{originVehicle: string, oirginCompanyVat:
   currenType: 0, // 開啟確認提示框時紀錄目前type
   invoiceItems: null
 })
-const {originVehicle, oirginCompanyVat, originCompanyName, isVehicleSetting, isCompanySetting, currenType, invoiceItems} = toRefs(props)
+const { originVehicle, oirginCompanyVat, originCompanyName, isVehicleSetting, isCompanySetting, currenType, invoiceItems } = toRefs(props)
 
-//重置button的字體顏色
-const resetBtnTextColor = computed(() => {
-  if (currenType.value === 5 || currenType.value === 7) {
-    const isSetting = currenType.value === 5 ? isVehicleSetting.value : isCompanySetting.value;
-    return isSetting ? "text-c_red" : "text-c_sliver";
-  }
-  return "text-c_sliver";
-});
-
-//重置button的border顏色
-const resetBtnBorderColor = computed(() => {
-  if (currenType.value === 5 || currenType.value === 7) {
-    const isSetting = currenType.value === 5 ? isVehicleSetting.value : isCompanySetting.value;
-    return isSetting ? "border-c_red" : "border-c_sliver";
-  }
-  return "border-c_sliver";
-});
-
-//
-const dialogTitle = computed(()=> {
-  return invoiceItems.value?.find(v=>v.type === currenType.value)?.typeName || "修改發票資訊"
+//popup title
+const dialogTitle = computed(() => {
+  return invoiceItems.value?.find(v => v.type === currenType.value)?.typeName || "修改發票資訊"
 })
+
+//設定reset Button樣式
+watch(currenType, (currenType) => {
+  const isSetting = currenType === 5 ? isVehicleSetting.value : isCompanySetting.value
+  resetBtnTextColor.value = isSetting ? "text-c_red" : "text-c_sliver"
+  resetBtnBorderColor.value = isSetting ? 'border-c_red' : "border-c_sliver"
+},{immediate: true})
 
 const emit = defineEmits(['getInvoiceList'])
 
-const closeDialog:any = inject("closeDialog") //關閉dialog方法
+const closeDialog: any = inject("closeDialog") //關閉dialog方法
 
 const popup = usePopup(); //popup composable
 
 
 init()
-function init(){
+function init() {
   //設定前值
   vehicle.value = originVehicle.value
   companyVat.value = oirginCompanyVat.value
@@ -115,7 +108,7 @@ async function updateInvoice(type: number) {
   switch (type) {
     case 5:
       //若沒修改則不需要檢查或打api
-      if(vehicle.value && vehicle.value === originVehicle.value) {
+      if (vehicle.value && vehicle.value === originVehicle.value) {
         closeDialog()
         return
       }
@@ -138,7 +131,8 @@ async function updateInvoice(type: number) {
 }
 
 //重置發票資訊
-async function resetInvoice(type: number){
+async function resetInvoice(type: number) {
+
   const confirm = await popup.confirm('是否將該筆發票資訊刪除');
   if (!confirm) return;
   await api.invoice.deleteInvoice(type)
@@ -149,9 +143,9 @@ async function resetInvoice(type: number){
 // 驗證手機載具
 async function verifyVehicle() {
   //先驗證長度
-  if(vehicle.value?.length<8) {
+  if (vehicle.value?.length < 8) {
     isVehicleValid.value = false
-    return 
+    return
   }
   const v = "/" + vehicle.value.replace(/[^0-9A-Z.+-]/g, "") //去除英數字.+-以外字元(開頭/也會被去掉所以要加回來)
   isVehicleValid.value = await api.invoice.verifyVehicle(v)
@@ -164,4 +158,3 @@ watch(vehicle, (value) => {
 }, { immediate: true });
 
 </script>
-
